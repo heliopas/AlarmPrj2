b1a0aef2147d719ce5871c6e146385fd871c5791
RompkoH <helio.rompkovski@landisgyr.com>
2024-12-19 16:32:00 -0300

    Arquivo README atualizado Kotlin prj

--- KotlinAppMqtt/README.md
+++ KotlinAppMqtt/README.md
@@ -1,1 +1,27 @@
+# SmartRelay
+Interface Kotlin para ativar e desativar funções de alarme
+
+Tabela de conteúdos
+=================
+<!--ts-->
+* [Sobre](#Sobre)
+* [Funções disponíveis](#Features)
+
+<!--te-->
+
+## Sobre
+Interface para acionamento para alarme usando Mqtt
+
+## Funções disponíveis
+- [ ] MqTT open port
+
+## Libs
+
+## Features and release notes
+
 19/12/2024 - Corrigido bug Appcompact Activity
+19/12/2024 - Adicionado função connect 'quebra execução ao clicar connect'
+
+## Exemplos
+
+*https://www.emqx.com/en/blog/android-connects-mqtt-using-kotlin
--- KotlinAppMqtt/app/src/main/java/com/example/alarmprjapp/ui/activity/MainActivity.kt
+++ KotlinAppMqtt/app/src/main/java/com/example/alarmprjapp/ui/activity/MainActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.service.controls.actions.FloatAction
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,14 +24,65 @@ import androidx.compose.ui.tooling.preview.Preview
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.MqttClient
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.eclipse.paho.client.mqttv3.IMqttActionListener
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.IMqttToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
 
 class MainActivity : AppCompatActivity(R.layout.mainpage) {
 
 
-    private lateinit var mqttClient: MqttClient
+    private lateinit var mqttClient: MqttAndroidClient
 
     companion object {
         const val TAG = "AndroidMqttClient"
+    }
+
+
+    fun connect(context: Context)
+    {
+
+        val serverMqtt = "broker.emqx.io"
+        mqttClient = MqttAndroidClient(context, serverMqtt, "hpr")
+
+        mqttClient.setCallback(object : MqttCallback{
+
+            override fun messageArrived(topic: String?, message: MqttMessage?) {
+                Log.d(TAG, "Receive message: ${message.toString()} from topic: $topic")
+            }
+
+            override fun connectionLost(cause: Throwable?) {
+                Log.d(TAG, "Connection lost ${cause.toString()}")
+            }
+
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+
+            }
+
+        })
+
+        val options = MqttConnectOptions()
+
+        try {
+            mqttClient.connect(options, null, object : IMqttActionListener{
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "Connection success")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(TAG, "Connection failure")
+                }
+            })
+        }catch (e : MqttException){
+            e.printStackTrace()
+
+        }
+
+
+
     }
 
     @SuppressLint("WrongViewCast", "MissingInflatedId")
@@ -37,16 +90,13 @@ class MainActivity : AppCompatActivity(R.layout.mainpage) {
         super.onCreate(savedInstanceState)
 
 
-//        val mqttConnect = findViewById<FloatingActionButton>(R.id.MqttConnect)
-//        mqttConnect.setOnClickListener {
-//            fun connect(context : Context){
-//
-//
-//
-//
-//
-//            }
-//        }
+        val mqttConnect = findViewById<Button>(R.id.MqttConnect)
+        mqttConnect.setOnClickListener {
+            Log.i(TAG, "Mqtt connect pressed!!!!")
+
+            connect(context = this)
+
+        }
 
     }
 
