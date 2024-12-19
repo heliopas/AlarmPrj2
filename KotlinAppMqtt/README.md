0c6d7861a2182e5172c7e91752276b38b437dabc
RompkoH <helio.rompkovski@landisgyr.com>
2024-12-19 15:19:40 -0300

    Arquivo readme app Android atualizado

---
+++ KotlinAppMqtt/README.md
@@ -0,0 +1,1 @@
+19/12/2024 - Corrigido bug Appcompact Activity
--- KotlinAppMqtt/app/src/main/java/com/example/alarmprjapp/ui/activity/MainActivity.kt
+++ KotlinAppMqtt/app/src/main/java/com/example/alarmprjapp/ui/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.alarmprjapp
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.service.controls.actions.FloatAction
 import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -10,6 +12,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,26 +21,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.MqttClient
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(R.layout.mainpage) {
 
+
+    private lateinit var mqttClient: MqttClient
+
+    companion object {
+        const val TAG = "AndroidMqttClient"
+    }
 
     @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
 
 
-        setContentView(R.layout.mainpage)
-
-
-
-
-
-
-
-
-
+//        val mqttConnect = findViewById<FloatingActionButton>(R.id.MqttConnect)
+//        mqttConnect.setOnClickListener {
+//            fun connect(context : Context){
+//
+//
+//
+//
+//
+//            }
+//        }
 
     }
+
+
+
 }
 
